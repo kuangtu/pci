7b1c9001e28f0f116051a5350decaa22dd216815
moxuansheng <moxuansheng@gmail.com>
2017-10-05 14:08:52 +0800

    add test cluster

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from PIL import Image,ImageDraw
 
 def readfile(filename):
@@ -26,7 +27,7 @@ def pearson(v1,v2):
   # Sums of the squares
   sum1Sq=sum([pow(v,2) for v in v1])
   sum2Sq=sum([pow(v,2) for v in v2])	
-  
+
   # Sum of the products
   pSum=sum([v1[i]*v2[i] for i in range(len(v1))])
   
@@ -176,19 +177,30 @@ def kcluster(rows,distance=pearson,k=4):
   ranges=[(min([row[i] for row in rows]),max([row[i] for row in rows])) 
   for i in range(len(rows[0]))]
 
+  #ranges包含了所有单词出现频率的最大、最小值
+  #rows[0]单词数目， row in rows每个博客
+
   # Create k randomly placed centroids
+  #相当于随机产生另外k个blog
+
+  #ranges[i][1] - ranges[i][0]之间，即：最大最小值之间
   clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] 
   for i in range(len(rows[0]))] for j in range(k)]
+
+  print len(clusters)
   
   lastmatches=None
   for t in range(100):
+    #初始时认为随机生成的k个blog是中心点
     print 'Iteration %d' % t
     bestmatches=[[] for i in range(k)]
-    
+
     # Find which centroid is the closest for each row
+    #遍历所有的blog
     for j in range(len(rows)):
       row=rows[j]
       bestmatch=0
+      #在随机生成的k个blog中，查找数据集合中与该glog距离最近的
       for i in range(k):
         d=distance(clusters[i],row)
         if d<distance(clusters[bestmatch],row): bestmatch=i
@@ -200,12 +212,17 @@ def kcluster(rows,distance=pearson,k=4):
     
     # Move the centroids to the average of their members
     for i in range(k):
+      #把中心点移到所有成员的平均位置
       avgs=[0.0]*len(rows[0])
+      print avgs
       if len(bestmatches[i])>0:
+        #获取k个随机分组中包含的所有“点”
         for rowid in bestmatches[i]:
+          # 对于每行数据进行相加
           for m in range(len(rows[rowid])):
             avgs[m]+=rows[rowid][m]
         for j in range(len(avgs)):
+          #然后除以行数，取得平均值
           avgs[j]/=len(bestmatches[i])
         clusters[i]=avgs
       
---
+++ chapter3/testCluster.py
@@ -0,0 +1,53 @@
+#coding:utf-8
+
+import clusters
+
+def getBlogData():
+    rows = 1
+    cols = 2
+    blognames, words, data = clusters.readfile('blogdata.txt')
+
+    print blognames[rows]
+    print words[cols]
+    print data[rows][cols]
+
+    clust = clusters.hcluster(data)
+
+    clusters.printclust(clust, blognames)
+
+
+    # v1 = [1,2,3]
+    #
+    # v2 = [0,3,3]
+    # a = clusters.pearson(v1, v2)
+    # print a
+    #
+    # distances = {}
+    # distances[(1,2)] = 3
+    # print distances
+    # distances[(2,1)] = 4
+    # print distances
+    # lowestpair = (1,2)
+    # print lowestpair[0]
+
+def getKCluster():
+    blognames, words, data = clusters.readfile('blogdata.txt')
+    # num = [data[i][0] for i in range(len(blognames))]
+    # print num
+
+    kclust = clusters.kcluster(data, k=10)
+    print kclust
+
+    # print len(data[0])
+    # print len(data)
+    # for row in data:
+    #     print row
+    #     print '\n'
+    # ranges = [(min([row[i] for row in data]), max([row[i] for row in data]))
+    #           for i in range(len(data[0]))]
+    #
+    # print type(ranges)
+    # print ranges[1]
+if __name__ == '__main__':
+    # getBlogData()
+    getKCluster()
